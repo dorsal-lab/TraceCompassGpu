a682751fa50b2323125166e14140f8d2c43e0bbc
Sebastien Darche <sebastien.darche@polymtl.ca>
2022-06-14 15:56:41 -0400

    Init GPU Roofline view

---
+++ org.eclipse.tracecompass.incubator.gpu.ui/src/org/eclipse/tracecompass/incubator/gpu/ui/views/GpuRooflineView.java
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+package org.eclipse.tracecompass.incubator.gpu.ui.views;
+
+import org.eclipse.tracecompass.tmf.ui.views.TmfView;
+
+/**
+ * @author Sébastien Darche <sebastien.darche@polymtl.ca>
+ *
+ */
+public class GpuRooflineView extends TmfView {
+
+    public GpuRooflineView(String viewName) {
+        super(viewName);
+    }
+
+    @Override
+    public void setFocus() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
