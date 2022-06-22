f6dd90bf7733d3dab03ee9a67fc922a123c3724d
Sebastien Darche <sebastien.darche@polymtl.ca>
2022-06-22 14:55:45 -0400

    Dumb id typo cost me 3 days of work

--- org.eclipse.tracecompass.incubator.gpu.core/src/org/eclipse/tracecompass/incubator/gpu/analysis/GpuRooflineDataProviderFactory.java
+++ org.eclipse.tracecompass.incubator.gpu.core/src/org/eclipse/tracecompass/incubator/gpu/analysis/GpuRooflineDataProviderFactory.java
@@ -3,12 +3,19 @@
  */
 package org.eclipse.tracecompass.incubator.gpu.analysis;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.tracecompass.internal.tmf.core.model.DataProviderDescriptor;
+import org.eclipse.tracecompass.tmf.core.dataprovider.IDataProviderDescriptor;
+import org.eclipse.tracecompass.tmf.core.dataprovider.IDataProviderDescriptor.ProviderType;
 import org.eclipse.tracecompass.tmf.core.dataprovider.IDataProviderFactory;
 import org.eclipse.tracecompass.tmf.core.model.tree.ITmfTreeDataModel;
 import org.eclipse.tracecompass.tmf.core.model.tree.ITmfTreeDataProvider;
 import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
+import org.eclipse.tracecompass.tmf.core.trace.TmfTraceUtils;
 
 /**
  * @author Sébastien Darche <sebastien.darche@polymtl.ca>
@@ -16,9 +23,22 @@ import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
  */
 public class GpuRooflineDataProviderFactory implements IDataProviderFactory {
 
+    private static final IDataProviderDescriptor DESCRIPTOR = new DataProviderDescriptor.Builder()
+            .setId(GpuRooflineAnalysisDataProvider.ID)
+            .setName("GPU Roofline data provider") //$NON-NLS-1$
+            .setDescription("GPU Roofline data provider") //$NON-NLS-1$
+            .setProviderType(ProviderType.TREE_TIME_XY)
+            .build();
+
     @Override
     public @Nullable ITmfTreeDataProvider<? extends ITmfTreeDataModel> createProvider(@NonNull ITmfTrace trace) {
         return GpuRooflineAnalysisDataProvider.create(trace);
+    }
+
+    @Override
+    public Collection<IDataProviderDescriptor> getDescriptors(@NonNull ITmfTrace trace) {
+        GpuRooflineAnalysis module = TmfTraceUtils.getAnalysisModuleOfClass(trace, GpuRooflineAnalysis.class, GpuRooflineAnalysis.ID);
+        return module != null ? Collections.singletonList(DESCRIPTOR) : Collections.EMPTY_LIST;
     }
 
 }
--- org.eclipse.tracecompass.incubator.gpu.ui/src/org/eclipse/tracecompass/incubator/gpu/ui/views/GpuRooflineView.java
+++ org.eclipse.tracecompass.incubator.gpu.ui/src/org/eclipse/tracecompass/incubator/gpu/ui/views/GpuRooflineView.java
@@ -5,10 +5,21 @@ package org.eclipse.tracecompass.incubator.gpu.ui.views;
 
 import org.eclipse.tracecompass.incubator.gpu.analysis.GpuRooflineAnalysis;
 import org.eclipse.tracecompass.incubator.gpu.analysis.GpuRooflineAnalysisDataProvider;
+import org.eclipse.tracecompass.tmf.ui.viewers.TmfViewer;
+import org.eclipse.tracecompass.tmf.ui.viewers.tree.AbstractSelectTreeViewer2;
+import org.eclipse.tracecompass.tmf.ui.viewers.tree.ITmfTreeColumnDataProvider;
+import org.eclipse.tracecompass.tmf.ui.viewers.tree.TmfTreeColumnData;
+import org.eclipse.tracecompass.tmf.ui.viewers.tree.TmfTreeViewerEntry;
 import org.eclipse.tracecompass.tmf.ui.viewers.xychart.TmfXYChartViewer;
 import org.eclipse.tracecompass.tmf.ui.viewers.xychart.linechart.TmfFilteredXYChartViewer;
 import org.eclipse.tracecompass.tmf.ui.viewers.xychart.linechart.TmfXYChartSettings;
 import org.eclipse.tracecompass.tmf.ui.views.xychart.TmfChartView;
+import com.google.common.collect.ImmutableList;
+import java.util.Comparator;
+import java.util.Objects;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.swtchart.Chart;
 import org.eclipse.swtchart.IAxis;
@@ -62,4 +73,23 @@ public class GpuRooflineView extends TmfChartView {
 
         return chartViewer;
     }
+
+    private static final class TreeViewer extends AbstractSelectTreeViewer2 {
+        public TreeViewer(Composite parent) {
+            super(parent, 1, GpuRooflineAnalysisDataProvider.ID);
+            }
+
+
+        @Override
+        protected ITmfTreeColumnDataProvider getColumnDataProvider() {
+            return () -> ImmutableList.of(createColumn("Name", Comparator.comparing(TmfTreeViewerEntry::getName)), //$NON-NLS-1$
+                    new TmfTreeColumnData("Legend")); //$NON-NLS-1$
+        }
+    }
+
+    @Override
+    protected @NonNull TmfViewer createLeftChildViewer(@Nullable Composite parent) {
+        return new TreeViewer(Objects.requireNonNull(parent));
+    }
+
 }
