29f5b4b8ff1999da41d02e31e74dd4a075ec7714
Sebastien Darche <sebastien.darche@polymtl.ca>
2022-06-28 14:01:41 -0400

    Remove unused types

--- org.eclipse.tracecompass.incubator.gpu.core/src/org/eclipse/tracecompass/incubator/gpu/core/trace/GpuInfo.java
+++ org.eclipse.tracecompass.incubator.gpu.core/src/org/eclipse/tracecompass/incubator/gpu/core/trace/GpuInfo.java
@@ -4,14 +4,11 @@
 package org.eclipse.tracecompass.incubator.gpu.core.trace;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collection;
 import java.util.List;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 /** @class GpuInfo
  * @brief Holds information about the offload device (peak bandwidth, ..)
@@ -35,14 +32,6 @@ public class GpuInfo {
     // ----- Attributes ----- //
 
     // Types
-
-    private static Type memoryRoofCollectionType = new TypeToken<Collection<MemoryRoof>>() {
-    }.getType();
-
-    private static Type computeRoofCollectionType = new TypeToken<Collection<ComputeRoof>>() {
-    }.getType();
-
-    // Attributes
 
     public String name;
     public List<MemoryRoof> memory_roofs;
