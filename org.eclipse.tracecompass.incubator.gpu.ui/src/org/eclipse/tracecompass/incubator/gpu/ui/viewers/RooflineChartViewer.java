d51afa3be02f8efb3906c6ce8070dde1a3e0640d
Sebastien Darche <sebastien.darche@polymtl.ca>
2022-07-04 10:58:05 -0400

    Substitute CommonXAxisChart

---
+++ org.eclipse.tracecompass.incubator.gpu.ui/src/org/eclipse/tracecompass/incubator/gpu/ui/viewers/RooflineChartViewer.java
@@ -0,0 +1,83 @@
+/**
+ *
+ */
+package org.eclipse.tracecompass.incubator.gpu.ui.viewers;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swtchart.Chart;
+import org.eclipse.swtchart.ICustomPaintListener;
+import org.eclipse.tracecompass.tmf.ui.viewers.xychart.TmfXYChartViewer;
+
+/**
+ * @author Sébastien Darche <sebastien.darche@polymtl.ca>
+ *
+ */
+public class RooflineChartViewer extends TmfXYChartViewer {
+
+    public RooflineChartViewer(Composite parent, String title, String id) {
+        super(parent, title, "", "");
+
+        // Quick hack to replace the swt chart
+        Composite fCommonComposite = getSwtChart().getParent();
+        getSwtChart().dispose();
+
+        Chart fSwtChart = new Chart(fCommonComposite, SWT.NONE);
+
+        fSwtChart.getPlotArea().addCustomPaintListener(new ICustomPaintListener() {
+
+            @Override
+            public void paintControl(PaintEvent e) {
+                drawGridLines(e.gc);
+            }
+
+            @Override
+            public boolean drawBehindSeries() {
+                return true;
+            }
+        });
+
+        setSwtChart(fSwtChart); // TA-DAAA remove useless listeners
+    }
+
+    private void drawGridLines(GC gc) {
+        Chart fSwtChart = getSwtChart();
+        Point size = fSwtChart.getPlotArea().getSize();
+        Color foreground = fSwtChart.getAxisSet().getXAxis(0).getGrid().getForeground();
+        gc.setForeground(foreground);
+        gc.setAlpha(foreground.getAlpha());
+        gc.setLineStyle(SWT.LINE_DOT);
+
+        /*
+        for (int x : fTimeScaleCtrl.getTickList()) {
+            gc.drawLine(x, 0, x,  size.y);
+        }
+        */
+
+        gc.setAlpha(255);
+    }
+
+    @Override
+    public Control getControl() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void refresh() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected void updateContent() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
--- org.eclipse.tracecompass.incubator.gpu.ui/src/org/eclipse/tracecompass/incubator/gpu/ui/views/GpuRooflineView.java
+++ org.eclipse.tracecompass.incubator.gpu.ui/src/org/eclipse/tracecompass/incubator/gpu/ui/views/GpuRooflineView.java
@@ -5,6 +5,7 @@ package org.eclipse.tracecompass.incubator.gpu.ui.views;
 
 import org.eclipse.tracecompass.incubator.gpu.analysis.GpuRooflineAnalysis;
 import org.eclipse.tracecompass.incubator.gpu.analysis.GpuRooflineAnalysisDataProvider;
+import org.eclipse.tracecompass.incubator.gpu.ui.viewers.RooflineChartViewer;
 import org.eclipse.tracecompass.tmf.ui.viewers.TmfViewer;
 import org.eclipse.tracecompass.tmf.ui.viewers.tree.AbstractSelectTreeViewer2;
 import org.eclipse.tracecompass.tmf.ui.viewers.tree.ITmfTreeColumnDataProvider;
@@ -36,7 +37,6 @@ public class GpuRooflineView extends TmfChartView {
 
     private static final String VIEW_ID = GpuRooflineAnalysis.ROOFLINE_VIEW_ID;
 
-
     /**
      * @brief Defaut constructor
      */
@@ -45,7 +45,8 @@ public class GpuRooflineView extends TmfChartView {
     }
 
     /**
-     * @param viewName View name
+     * @param viewName
+     *            View name
      */
     public GpuRooflineView(String viewName) {
         super(viewName);
@@ -54,7 +55,10 @@ public class GpuRooflineView extends TmfChartView {
     @Override
     protected TmfXYChartViewer createChartViewer(Composite parent) {
         TmfXYChartSettings settings = new TmfXYChartSettings(null, null, null, 1);
-        TmfFilteredXYChartViewer chartViewer = new TmfFilteredXYChartViewer(parent, settings, GpuRooflineAnalysisDataProvider.ID);
+        // TmfFilteredXYChartViewer chartViewer = new
+        // TmfFilteredXYChartViewer(parent, settings,
+        // GpuRooflineAnalysisDataProvider.ID);
+        TmfXYChartViewer chartViewer = new RooflineChartViewer(parent, "", GpuRooflineAnalysisDataProvider.ID); //$NON-NLS-1$
 
         Chart chart = chartViewer.getSwtChart();
 
@@ -78,8 +82,7 @@ public class GpuRooflineView extends TmfChartView {
     private static final class TreeViewer extends AbstractSelectTreeViewer2 {
         public TreeViewer(Composite parent) {
             super(parent, 1, GpuRooflineAnalysisDataProvider.ID);
-            }
-
+        }
 
         @Override
         protected ITmfTreeColumnDataProvider getColumnDataProvider() {
