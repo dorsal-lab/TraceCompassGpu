bb9b3613bc9c2523ed17695758b56ce007843871
Sebastien Darche <sebastien.darche@polymtl.ca>
2022-07-07 15:46:04 -0400

    build function name map (hsa correlation id)

--- org.eclipse.tracecompass.incubator.gpu.core/src/org/eclipse/tracecompass/incubator/gpu/analysis/GpuRooflineStateProvider.java
+++ org.eclipse.tracecompass.incubator.gpu.core/src/org/eclipse/tracecompass/incubator/gpu/analysis/GpuRooflineStateProvider.java
@@ -3,6 +3,8 @@
  */
 package org.eclipse.tracecompass.incubator.gpu.analysis;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import org.eclipse.jdt.annotation.NonNull;
@@ -12,6 +14,8 @@ import org.eclipse.tracecompass.incubator.gpu.core.trace.HipTrace;
 import org.eclipse.tracecompass.incubator.gpu.core.trace.KernelConfiguration;
 import org.eclipse.tracecompass.statesystem.core.ITmfStateSystemBuilder;
 import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
+import org.eclipse.tracecompass.tmf.core.event.ITmfEventField;
+import org.eclipse.tracecompass.tmf.core.event.TmfEventField;
 import org.eclipse.tracecompass.tmf.core.statesystem.AbstractTmfStateProvider;
 import org.eclipse.tracecompass.tmf.core.statesystem.ITmfStateProvider;
 import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
@@ -33,6 +37,8 @@ public class GpuRooflineStateProvider extends AbstractTmfStateProvider {
     private long totalFloatLoads = 0L;
     private long totalFloatStores = 0L;
 
+    private Map<Long, String> functionMap;
+
     /**
      * @param trace
      *            Trace
@@ -44,6 +50,7 @@ public class GpuRooflineStateProvider extends AbstractTmfStateProvider {
         kernelConf = _kernelConf;
         report = _report;
         gpuInfo = _gpuInfo;
+        functionMap = new HashMap<>();
     }
 
     @Override
@@ -58,10 +65,29 @@ public class GpuRooflineStateProvider extends AbstractTmfStateProvider {
 
     @Override
     protected void eventHandle(@NonNull ITmfEvent event) {
-        if (!event.getType().getName().equals(HipTrace.HIPTRACE_NAME)) {
-            return;
+        switch (event.getType().getName()) {
+        case HipTrace.HIPTRACE_NAME:
+            handleHipTraceEvent(event);
+            break;
+        case "hsa_function_name": //$NON-NLS-1$
+            handleHsaFunctionNameEvent(event);
+            break;
+        case "hsa_api": //$NON-NLS-1$
+            handleHsaApiEvent(event);
+            break;
+        default:
+            break;
         }
 
+    }
+
+    /**
+     * Handles a single Hip Trace event (counters)
+     *
+     * @param event
+     *            The event, assumed to be a valid hiptrace event
+     */
+    protected void handleHipTraceEvent(@NonNull ITmfEvent event) {
         long ts = event.getTimestamp().getValue();
 
         long counter = (long) event.getContent().getField("counter").getValue(); //$NON-NLS-1$
@@ -85,6 +111,31 @@ public class GpuRooflineStateProvider extends AbstractTmfStateProvider {
 
         quark = ss.getQuarkAbsoluteAndAdd("floating_s"); //$NON-NLS-1$
         ss.modifyAttribute(ts, totalFloatStores, quark);
+    }
+
+    /**
+     * Handles a name definition event ("hsa_function_name") from the ROCm trace
+     *
+     * @param event
+     *            The event, assumed to be a hsa_fucntion_name event
+     */
+    protected void handleHsaFunctionNameEvent(@NonNull ITmfEvent event) {
+        ITmfEventField root = event.getContent();
+        Long cid = (Long) root.getField("correlation_id").getValue();
+        String name = (String) root.getField("name").getValue();
+
+        functionMap.put(cid, name);
+    }
+
+    protected void handleHsaApiEvent(@NonNull ITmfEvent event) {
+        ITmfEventField root = event.getContent();
+        Long cid = (Long) root.getField("cid").getValue();
+        String func = functionMap.get(cid);
+
+        if(func == null) {
+            // Unknown function, do not process
+            return;
+        }
     }
 
 }
