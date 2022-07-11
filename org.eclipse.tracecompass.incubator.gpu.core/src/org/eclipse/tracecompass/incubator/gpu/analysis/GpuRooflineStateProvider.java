6e01f294f8b2b92272e8f65ed5e4c9de2fa1794c
Sebastien Darche <sebastien.darche@polymtl.ca>
2022-07-11 15:27:08 -0400

    Stub to handle different rocprof traces

--- org.eclipse.tracecompass.incubator.gpu.core/src/org/eclipse/tracecompass/incubator/gpu/analysis/GpuRooflineStateProvider.java
+++ org.eclipse.tracecompass.incubator.gpu.core/src/org/eclipse/tracecompass/incubator/gpu/analysis/GpuRooflineStateProvider.java
@@ -15,10 +15,10 @@ import org.eclipse.tracecompass.incubator.gpu.core.trace.KernelConfiguration;
 import org.eclipse.tracecompass.statesystem.core.ITmfStateSystemBuilder;
 import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
 import org.eclipse.tracecompass.tmf.core.event.ITmfEventField;
-import org.eclipse.tracecompass.tmf.core.event.TmfEventField;
 import org.eclipse.tracecompass.tmf.core.statesystem.AbstractTmfStateProvider;
 import org.eclipse.tracecompass.tmf.core.statesystem.ITmfStateProvider;
 import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
+import org.eclipse.tracecompass.tmf.core.util.Pair;
 
 /**
  * @author Sébastien Darche <sebastien.darche@polymtl.ca>
@@ -37,13 +37,33 @@ public class GpuRooflineStateProvider extends AbstractTmfStateProvider {
     private long totalFloatLoads = 0L;
     private long totalFloatStores = 0L;
 
-    private Map<Long, String> functionMap;
+    private Map<Pair<String, Long>, String> functionMap;
+
+    /**
+     * @brief Supported ROCm traces
+     */
+    protected static class APIType {
+        /**
+         * @brief HIP Trace activity ("--hip-trace")
+         */
+        public static String HIP_API = "hip"; //$NON-NLS-1$
+        /**
+         * @brief HSA Runtime tracing activity ("--hsa-trace")
+         */
+        public static String HSA_API = "hsa"; //$NON-NLS-1$
+    }
 
     /**
      * @param trace
      *            Trace
      * @param id
      *            Identifier
+     * @param _kernelConf
+     *            Kernel call configuration
+     * @param _report
+     *            HIP Analyzer report
+     * @param _gpuInfo
+     *            GPU Information
      */
     public GpuRooflineStateProvider(@NonNull ITmfTrace trace, @NonNull String id, KernelConfiguration _kernelConf, HipAnalyzerReport _report, GpuInfo _gpuInfo) {
         super(trace, id);
@@ -69,11 +89,22 @@ public class GpuRooflineStateProvider extends AbstractTmfStateProvider {
         case HipTrace.HIPTRACE_NAME:
             handleHipTraceEvent(event);
             break;
+        // HSA
         case "hsa_function_name": //$NON-NLS-1$
-            handleHsaFunctionNameEvent(event);
+            handlefunctionNameEvent(event, APIType.HSA_API);
             break;
         case "hsa_api": //$NON-NLS-1$
             handleHsaApiEvent(event);
+            break;
+        case "hip_function_name": //$NON-NLS-1$
+            handlefunctionNameEvent(event, APIType.HIP_API);
+            break;
+        case "hip_activity": //$NON-NLS-1$
+            break;
+        case "hip_api": //$NON-NLS-1$
+            handleHipApiEvent(event);
+            break;
+        case "kernel_event": //$NON-NLS-1$
             break;
         default:
             break;
@@ -118,21 +149,52 @@ public class GpuRooflineStateProvider extends AbstractTmfStateProvider {
      *
      * @param event
      *            The event, assumed to be a hsa_fucntion_name event
+     * @param prefix
+     *            API prefix ({@link APIType}
      */
-    protected void handleHsaFunctionNameEvent(@NonNull ITmfEvent event) {
+    protected void handlefunctionNameEvent(@NonNull ITmfEvent event, String prefix) {
         ITmfEventField root = event.getContent();
-        Long cid = (Long) root.getField("correlation_id").getValue();
-        String name = (String) root.getField("name").getValue();
+        Long cid = (Long) root.getField("correlation_id").getValue(); //$NON-NLS-1$
+        String name = (String) root.getField("name").getValue(); //$NON-NLS-1$
 
-        functionMap.put(cid, name);
+        functionMap.put(new Pair<>(prefix, cid), name);
     }
 
+    private String getCorrelationName(Long cid, String prefix) {
+        Pair<String, Long> entry = new Pair<>(prefix, cid); // $NON-NLS-1$
+                                                            // //$NON-NLS-2$
+        return functionMap.get(entry);
+    }
+
+    /**
+     * Handles a tracepoint as a call to an hsa api function
+     *
+     * @param event
+     *            The event, assumed to be a hsa_api event
+     */
     protected void handleHsaApiEvent(@NonNull ITmfEvent event) {
         ITmfEventField root = event.getContent();
-        Long cid = (Long) root.getField("cid").getValue();
-        String func = functionMap.get(cid);
+        Long cid = (Long) root.getField("cid").getValue(); //$NON-NLS-1$
+        String func = getCorrelationName(cid, APIType.HSA_API);
 
-        if(func == null) {
+        if (func == null) {
+            // Unknown function, do not process
+            return;
+        }
+    }
+
+    /**
+     * Handles a tracepoint as a call to an hip api function
+     *
+     * @param event
+     *            The event, assumed to be a hip_api event
+     */
+    protected void handleHipApiEvent(@NonNull ITmfEvent event) {
+        ITmfEventField root = event.getContent();
+        Long cid = (Long) root.getField("cid").getValue(); //$NON-NLS-1$
+        String func = getCorrelationName(cid, APIType.HIP_API);
+
+        if (func == null) {
             // Unknown function, do not process
             return;
         }
